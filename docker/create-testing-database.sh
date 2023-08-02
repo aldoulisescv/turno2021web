825c1164ed3e1911a0b2494a4e0e1c82376f7828
Roberto <roberto.benitez.mich@gmail.com>
2023-08-02 10:03:59 -0600

    Solamente hice las modificaciones vistas durante la primer llamada el 1 de agosto de 2023, las cuales fueron borrar un 10 del código en el archivo docker-compose.yml porque no se reconocía y borrar las primeras 2 lineas del archivo create-testing-database.sh

--- docker/create-testing-database.sh
+++ docker/create-testing-database.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS testing;
     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
